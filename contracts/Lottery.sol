d0a7449533a529aa430ba402198587cc9e973562
JK1331 <jakubkubala3@gmail.com>
2022-10-21 15:32:37 +0200

    Adding lottery functions

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,16 +1,26 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-contract Lottery {
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Lottery is Ownable {
     uint256 public closingTime;
+    bool public betsOpen;
 
     constructor(uint256 _betPrice, uint256 _betFee) {
         
     }
 
-/// @param _closingTime target time in seconds expressed in epoch time for the bets to close
-    function openBets(uint256 _closingTime) public {
+    modifier whenBetsClosed() {
+        require(!betsOpen, "Lottery: Bets are not closed");
+        _;
+    }
+
+    /// @param _closingTime target time in seconds expressed in epoch time for the bets to close
+    function openBets(uint256 _closingTime) public onlyOwner {
         require(_closingTime > block.timestamp, "Lottery: Closing time must be in the future");
         closingTime = _closingTime;
+        betsOpen = true;
     }
+
 }
