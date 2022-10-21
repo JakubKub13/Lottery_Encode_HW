6e4d4382dea89eba9729117ed24e18cbff6e5817
JK1331 <jakubkubala3@gmail.com>
2022-10-21 14:49:01 +0200

    Refactoring infinitum token

--- contracts/InfinitumToken.sol
+++ contracts/InfinitumToken.sol
@@ -2,17 +2,18 @@
 pragma solidity 0.8.8;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract InfinitumToken is ERC20, AccessControl {
+contract InfinitumToken is ERC20, ERC20Burnable, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    constructor() ERC20("Infinitum token", "INFT") {
-        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
     }
 
-    function mint(address to, uint256 amount) public {
-        require(hasRole(MINTER_ROLE, msg.sender), "InfinitumToken: Caller is not the minter");
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(to, amount);
     }
 }
