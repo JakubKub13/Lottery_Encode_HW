5a41459cca309aba47945f658037769f8f17c6a1
JK1331 <jakubkubala3@gmail.com>
2022-10-24 15:50:36 +0200

    Adding script

--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -10,12 +10,11 @@ export interface networkConfigInfo {
 export const networkConfig: networkConfigInfo = {
     31337: {
         name: "localhost",
-        lotteryToken: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+        
     },
 
     5: {
         name: "goerli",
-        lotteryToken: "0x97cdf8AfD135a6DCb1BA07DAfcc0Aa3ef1CE6cd4",
         lottery: "0x9f64ab5fdD0919c6E777B07283afC88D93E785f4",
     },
 
